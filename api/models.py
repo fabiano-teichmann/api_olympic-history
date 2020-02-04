a3aaef7238e455288ede032c9efb5c36f6849da3
Fabiano Teichmann <fabiano.geek@gmail.com>
2020-02-03 21:02:02 -0300

    Define modelagem de dados

--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+SEX = (
+    ('M', 'M'),
+    ('F', 'F'),
+)
+
+
+class Athlete(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    sex = models.CharField(max_length=100, choices=SEX, default=None, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+class Events(models.Model):
+    age = models.IntegerField(blank=True, null=True)
+    height = models.IntegerField(blank=True, null=True)
+    weight = models.FloatField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    games = models.CharField(max_length=100)
+    season = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    sport = models.CharField(max_length=100)
+    event = models.CharField(max_length=100)
+    medal = models.CharField(max_length=20, blank=True, null=True)
+    athlete_id = models.IntegerField()
+    noc = models.CharField(max_length=3, blank=True, null=True)
+    team = models.CharField(max_length=60, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
